6d31a66a0a392e57c0fe42d4a0754d55d0f0e08b
Lucia <lucia.ruiz@marsibionics.com>
2023-05-25 15:43:15 +0200

    Basic connect app created. Added buttons and texts

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+
+const btnTxtConn = "Connect to server"
+const btnTextSend = "Send!"
+const txtWriteMsg = "Write message for server:"
+const txtResponse = "Response from server:"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyButton buttonText={ btnTxtConn }/>
+      
+      <Title titleText={txtWriteMsg}/>
+
+      <MyButton buttonText={btnTextSend}/>
+      
+      <Title titleText={txtResponse}/>
+      
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export function Title({ titleText }) {
+  return (
+    <Text style={styles.titleText}>{ titleText }</Text>
+  );
+}
+
+export function MyButton ({ buttonText }) {
+  return (
+    <Button
+      title={ buttonText}
+      onPress={() => alert("button was pressed")}
+    />
   );
 }
 
@@ -16,5 +43,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  titleText: {
+    color: "midnightblue",
+    fontSize: 16,
+    marginVertical: 15,
   },
 });
