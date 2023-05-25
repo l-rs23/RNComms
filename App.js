a368f1bbb27cf1bf295b580866fa478b93939285
Lucia <lucia.ruiz@marsibionics.com>
2023-05-25 17:30:45 +0200

    Added small functionality

--- App.js
+++ App.js
@@ -1,39 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View, Alert } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
-const btnTxtConn = "Connect to server"
-const btnTextSend = "Send!"
-const txtWriteMsg = "Write message for server:"
-const txtResponse = "Response from server:"
+const btnTxtConn = "Connect to server";
+const btnTextSend = "Send!";
+const txtWriteMsg = "Write message for server:";
+const txtResponse = "Response from server:";
+
+let responseTxt = "";
 
 export default function App() {
+  const [msgText, setMsgText] = useState("");
+  const [responseTxt, setResponseTxt] = useState("");
+
+  function MyButton ({ buttonText }) {
+    if (buttonText === btnTextSend) {
+      return (
+        <Button
+          title={ buttonText}
+          onPress={() => setResponseTxt(msgText)}
+        />
+      );
+    }
+  
+    return (
+      <Button
+        title={ buttonText}
+        onPress={() => alert("Button was pressed.")}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
       <MyButton buttonText={ btnTxtConn }/>
       
       <Title titleText={txtWriteMsg}/>
-
+      <TextInput
+        style={styles.inputText}
+        placeholder='Type a message'
+        onChangeText={(value) => setMsgText(value)}
+      />
       <MyButton buttonText={btnTextSend}/>
       
       <Title titleText={txtResponse}/>
-      
+      <Text style={styles.inputText}>
+        {responseTxt}
+      </Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-export function Title({ titleText }) {
+function Title({ titleText }) {
   return (
     <Text style={styles.titleText}>{ titleText }</Text>
-  );
-}
-
-export function MyButton ({ buttonText }) {
-  return (
-    <Button
-      title={ buttonText}
-      onPress={() => alert("button was pressed")}
-    />
   );
 }
 
@@ -48,6 +70,21 @@ const styles = StyleSheet.create({
   titleText: {
     color: "midnightblue",
     fontSize: 16,
+    marginTop: 25,
+  },
+
+  inputText: {
+    height: 50,
+    width: 350,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 3,
+    padding: 10,
     marginVertical: 15,
+  },
+
+  textBox: {
+    borderWidth: 1,
+    borderRadius: 3,
   },
 });
